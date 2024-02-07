2e0db1d7d59c7e4e37015ef779e56ec336731186
Milan Hauth <milahu@gmail.com>
2024-02-07 16:47:22 +0100

    set NIX_BUILD_DEBUG_ROOT

--- nix-build-debug.sh
+++ nix-build-debug.sh
@@ -2,4 +2,4 @@
 
 d=$(dirname "$0")
 
-exec nix-shell "$@" --run "bash --noprofile --rcfile ${d@Q}/nix-build-debug.env.sh"
+exec nix-shell "$@" --run "NIX_BUILD_DEBUG_ROOT=${PWD@Q} bash --noprofile --rcfile ${d@Q}/nix-build-debug.env.sh"
