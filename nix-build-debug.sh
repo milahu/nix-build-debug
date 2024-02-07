39fa855a1b477b30c62a77979eb637d5937b5e04
Milan Hauth <milahu@gmail.com>
2024-02-07 19:12:03 +0100

    env: make the build fail to not pollute the nix store

--- nix-build-debug.sh
+++ nix-build-debug.sh
@@ -10,19 +10,34 @@ d=$(readlink -f "$(dirname "$0")")
 pkgsPath="<nixpkgs>"
 pkgAttr="hello"
 
+env_json_start="################ env.json start ################"
+env_json_end="################ env.json end ################"
+
 nix_build_env_expr='
     with import '"$pkgsPath"' {};
     '"$pkgAttr"'.overrideAttrs (oldAttrs: {
         # TODO better
         name = "${oldAttrs.pname}-${oldAttrs.version}-env.json";
         outputs = ["out"];
-        builder = '"$d"/get-env.sh';
+        # create backup of buildCommand
+        buildCommand_bak_nix_build_debug =
+            if oldAttrs ? buildCommand then oldAttrs.buildCommand
+            else null;
+        buildCommand = '"''"'
+            source ${'"$d"/get-env.sh'}
+            echo "'"$env_json_start"'"
+            cat "$out"
+            echo
+            echo "'"$env_json_end"'"
+            exit 1 # make the build fail to not pollute the nix store
+        '"''"';
     })
 '
 echo "nix_build_env_expr:$nix_build_env_expr" >&2 # debug
 
-env_json_path=$(nix-build -E "$nix_build_env_expr")
-echo "env_json_path=$env_json_path" >&2 # debug
+env_json=$(nix-build -E "$nix_build_env_expr" 2>&1)
+env_json=$(echo "$env_json" | awk "/^$env_json_start$/{flag=1; next} /^$env_json_end$/{flag=0} flag")
+echo "env_json: $env_json" >&2 # debug
 
 echo "FIXME generate rcfile for bash" >&2
 exit
