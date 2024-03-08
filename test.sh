8ce4bc5bcf0a5b61c82f0c09bcbbed09455d84b0
Milan Hauth <milahu@gmail.com>
2024-03-08 12:33:51 +0100

    add tests

--- test.sh
+++ test.sh
@@ -3,3 +3,13 @@
 set -x
 
 exec ./nix-build-debug.sh '<nixpkgs>' -A hello "$@"
+
+# test buildPhase string
+./nix-build-debug.sh '<nixpkgs>' -A openfx
+
+# test buildCommand string
+./nix-build-debug.sh '<nixpkgs>' -A rpmextract
+
+# test buildCommandPath
+# passAsFile = [ "buildCommand" ];
+# this is almost never used
