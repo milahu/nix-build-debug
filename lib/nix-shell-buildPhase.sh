4dd3ff6b415b4788435817fbd90c2327c7161812
Milan Hauth <milahu@milahu.duckdns.org>
2026-05-15 13:23:02 +0200

    add nix-shell-buildPhase.sh

---
+++ lib/nix-shell-buildPhase.sh
@@ -0,0 +1,8 @@
+{ echo "------------------------------------------------------------";
+  echo " WARNING: the existence of this path is not guaranteed.";
+  echo " It is an internal implementation detail for pkgs.mkShell.";
+  echo "------------------------------------------------------------";
+  echo;
+  # Record all build inputs as runtime dependencies
+  export;
+} >> "$out"
